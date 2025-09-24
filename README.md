10023ef31739006bbeebfdb686454e13c4c59482
Vinícius de Matos Brandão <dtivini@gmail.com>
2025-09-23 23:07:40 -0300

    Timer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Timer
 A page with a timer to time whatever you want
+
+
+Font creator: Sizenko Alexander
+Font name: Digital-7
---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+function clock() {
+    function getTimeFromSeconds(seconds) {
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const clock = document.querySelector('.clock');
+    const start = document.querySelector('.start');
+    const pause = document.querySelector('.pause');
+    const restart = document.querySelector('.restart');
+    let timer;
+    let seconds = 0;
+
+    function startClock() {
+        timer = setInterval(function() {
+            seconds++;
+            clock.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000);
+    }
+
+    start.addEventListener('click', function() {
+        clock.classList.remove('stoped');
+        clearInterval(timer);
+        startClock();
+    });
+
+    pause.addEventListener('click', function() {
+        clock.classList.add('stoped')
+        setTimeout(function(){
+            clearInterval(timer);
+        });
+    });
+
+    restart.addEventListener('click', function() {
+        clearInterval(timer);
+        clock.classList.remove('stoped');
+        seconds = 0;
+        clock.innerHTML = "00:00:00";
+    });
+}
+
+clock();
